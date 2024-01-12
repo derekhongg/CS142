807e2841d4c990e279aaaa659e10d1242997e007
derekhongg <hongderek@gmail.com>
2024-01-11 20:51:15 -0800

    new commit

---
+++ Lecture01/Lecture01.java
@@ -0,0 +1,87 @@
+/**
+ * Lecture01
+ * Derek Hong
+ */
+public class Lecture01 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    System.out.println("BIT 142 Rocks!");
+    // Note: you will need to put some of your answers inside a comments
+    //
+    // Turn a line into a comment by putting // at the start of the line, like we're
+    // doing here
+    //
+    // You can comment a lot of lines at once in VSCode by
+    // highlighting the lines and then pressing Control/Command-/
+    // (or press Control/Command-P to bring up the Command Pallete and look for the
+    // 'Toggle Line Comment' command)
+
+    // Put your answer for #4 here:
+
+    /*
+     * 
+     * 1. Get ingredients
+     * 2. Pre-heat oven
+     * 3. Prep baking sheets
+     * 4. Prep Dry ingredients
+     * 5. Mix butter and sugar
+     * 6. Add eggs and vanilla
+     * 7. Mix wet and dry ingredients
+     * 8. Add chocolate chips (optional)
+     * 9. Scoop Dough onto sheets
+     * 10. Bake
+     * 11. Cool
+     * 12. Enjoy cookies
+     * 
+     */
+
+    // Put your answer for #5 here:
+    /*
+     * Java is the source code file. Contains readable code to create a java program
+     * 
+     * MyProgram.class is compiled in bytecode. It is what the JVM executes.
+     * 
+     */
+    // Put your answer for #6 here:
+    /*
+     * A
+     * D
+     * F
+     * H
+     * I
+     */
+    // please copy-and-paste the actual answers here (instead of listing A, D, R, Q,
+    // etc)
+
+    // Put your answer for #7 here: D
+    // please copy-and-paste the actual answers here (instead of listing A, D, R, Q,
+    // etc)
+
+    // Put your answer for #8 here:
+    /*
+     * Does this print quotes?
+     * What about this, does this print "quotes"?
+     * How '"confounding' "\" it is!
+     */
+    // Put your answer for #16 here:
+    System.out.println("Twas brillig and the");
+    System.out.println("slithy toves did gryre and");
+    System.out.println("gimble");
+    System.out.println();
+    System.out.println("in the wabe.");
+    // NOTE: This code should compile and run!
+    System.out.println("My answer for question #16:");
+    // (VSCode/Prettier isn't letting me put a blank line after the code so I'll put
+    // this comment in, instead)
+
+    // Put your answer for #20 here:
+    /* 
+    1. No opening bracket after public class
+    2. No arguments (String[] args) in public class
+    3. Second line didn't finish quotations
+    4. Line 8 didn't have closing parentheses
+    */
+  }
+}
