eea3b3e097904520aa6db32f4dba1db36ba114c1
derekhongg <hongderek@gmail.com>
2024-01-21 14:45:11 -0800

    new assignment added

--- Lecture02/AssignedAsciiArt.java
+++ Lecture02/AssignedAsciiArt.java
@@ -1,8 +1,6 @@
 public class AssignedAsciiArt {
-
-    // Declares a constant controlling
     // the size of the needle
-    public static final int SIZE = 4;
+    public static final int SIZE = 2;
 
     public static void main (String[] args) {
         needle();
@@ -19,20 +17,24 @@ public class AssignedAsciiArt {
         for (int rows = 1; rows <= SIZE; rows++) {
             //this will print the number of spaces before the needle starts
             for (int j = 0; j < 3*SIZE; j++) {
+                // there are 3 times size of spaces
                 System.out.print(" ");
             }
+            //once the spaces end, it will print the lines until the size stops
             System.out.println("||");
         }
     }
 
     public static void top() {
+        // the number of rows the dome will last is constant
         for (int rows = 1; rows <= SIZE; rows++) {
-            //
+            // ex. 3*3(SIZE) - 3*1 = 5 - num of spaces before __/
             for(int i = 1; i<=((3*SIZE) - (3*rows)); i++) {
                 System.out.print(" ");
             }
             //After the spaces for the loop end, it will start printing the top part "__/"
             System.out.print("__/");
+            //depending on the row, only prints a cerain amount of :
             for(int i = 1; i<=((3*rows) - ((3))); i++) {
                 System.out.print(":");
             }
@@ -46,7 +48,9 @@ public class AssignedAsciiArt {
     }
 
     public static void floor() {
+        // it always starts with a "|"
         System.out.print("|");
+        //always divisble by 6 and have to print the quotes
         for (int i = 1; i <= 6 * SIZE; i++) {
             System.out.print("\"");
         }
@@ -56,6 +60,7 @@ public class AssignedAsciiArt {
 
     public static void bottomDome() {
         for(int rows = 1; rows <= SIZE; rows++) {
+            // depending on the number of rows, it will only print a certain amount of lines
             for(int i = 1; i <= (2* rows) - 2; i++) {
                 System.out.print(" ");
             }
@@ -77,6 +82,7 @@ public class AssignedAsciiArt {
             for (int i = 0; i <= SIZE * 3 - SIZE; i++) {
                 System.out.print(" ");
             }
+            // this will add a percentage signs if the size is greater than 2
             for (int j = 1; j <= SIZE - 2; j++) {
                 percentageSign += "%";
             }
---
+++ Lecture03/Lecture03.java
@@ -0,0 +1,58 @@
+/* Lecture 03
+ * REPLACE THIS LINE WITH YOUR NAME,
+ * SO THE TEACHER KNOW WHO'S WORK THEY'RE GRADING :)
+*/
+public class Lecture03 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // SELF-CHECK PROBLEMS:
+    // Note: you will need to put some of your answers inside a comments
+
+    // Put your answer for #3A here:
+
+    // Put your answer for #3B here:
+
+    // Put your answer for #3C here:
+
+    // Put your answer for #3D here:
+
+    // Put your answer for #3E here:
+
+    // Put your answer for #3F here:
+
+    // Put your answer for #3F here:
+
+    // Put your answer for #7 here:
+    // Make sure that the code that you write here compiles and runs
+
+    // Put your answer for #18 here:
+    // Make sure that the code that you write here compiles and runs
+
+    // Put your answer for #21 in the static method
+    // named "Exercise_21_Finish_Me" down below
+    //
+    // In order for the auto-graded tests to pass you MUST uncomment the next line:
+    // Exercise_21_Finish_Me(); // _UN_COMMENT THIS LINE!!!!
+
+    // Put your answer for #25 here:
+
+    // EXERCISES:
+
+    // Put your answer for #4 here:
+
+    // Put your answer for #5 here:
+
+    // Put your answer for #6 here:
+  }
+
+  // In VSCode you can press Ctrl/Cmd + / to uncomment a bunch of lines at once
+  // (select the lines, then Ctrl+/)
+  // public static void Exercise_21_Finish_Me() {
+  // for (int i = /* FINISH ME */) {
+  // System.out.println(/* FINISH ME */);
+  // }
+  // }
+
+}
