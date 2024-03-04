8e59ed1e294d5f31bc660716335974b4aaf8a260
Derek Hong <90344107+derekhongg@users.noreply.github.com>
2024-03-04 15:22:08 -0800

    Create Lecture18.java

---
+++ Lecture17/Lecture18.java
@@ -0,0 +1,27 @@
+public class Lecture18 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // SELF-CHECK PROBLEMS:
+    // Note: you will need to put some of your answers inside a comments
+
+    // Put your answer for #22 here:
+
+    // Put your answer for #23 here:
+
+    // Put your answer for #24 here:
+
+    // Put your answer for #25 here:
+
+    // Put your answer for #26 here:
+
+    // EXERCISES:
+
+    // Put your answer for #15 in this repo:
+
+    // Put your answer for #16 in this repo:
+
+    // Put your answer for #18 in this repo:
+  }
+}
