bac0f63015431adbf7a43fac942e88af8443cc51
Derek Hong <90344107+derekhongg@users.noreply.github.com>
2024-03-04 15:25:54 -0800

    Update Lecture18.java

--- Lecture17/Lecture18.java
+++ Lecture17/Lecture18.java
@@ -7,9 +7,15 @@ public class Lecture18 {
     // Note: you will need to put some of your answers inside a comments
 
     // Put your answer for #22 here:
-
+      /*  
+      Implementing an interace means that a class agrees to proved the implementation for all the methods defined in that interface.
+      Extending a class means that a subclass inherits the properties and behaviors of the superclass, including its methods, variables, and implementation.
+      */
     // Put your answer for #23 here:
-
+      /*
+      In order for the code to class C to compile successfully, it must provide implementations for all the methods defined in the interface. 
+      Class C must implement m1() and m2() methods delcared in the interface I.
+      */
     // Put your answer for #24 here:
 
     // Put your answer for #25 here:
